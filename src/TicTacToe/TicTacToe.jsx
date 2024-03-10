3568299af4f07236822c2b62f1c577c0c5bb5ee3
SHANIB C K <ckshanib8@gmail.com>
2024-03-10 10:39:04 +0530

    errors solved

--- src/TicTacToe/TicTacToe.jsx
+++ src/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import cross_icon from "../assets/cross.png";
 import circle_icon from "../assets/circle.png";
 import "./TicTacToe.css";
 
+let data = ["", "", "", "", "", "", "", "", ""];
+
 const TicTacToe = () => {
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
+
+  //   toggle function created section
+  const toggle = (e, num) => {
+    if (lock) {
+      return 0;
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${cross_icon}'>`;
+      data[num] = "x";
+      setCount(++count);
+    } else {
+      e.target.innerHTML = `<img src='${circle_icon}'>`;
+      data[num] = "o";
+      setCount(++count);
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -13,19 +34,64 @@ const TicTacToe = () => {
         {/* board section */}
         <div className="board">
           <div className="row1">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 0);
+              }}
+            ></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 1);
+              }}
+            ></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 2);
+              }}
+            ></div>
           </div>
           <div className="row2">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 3);
+              }}
+            ></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 4);
+              }}
+            ></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 5);
+              }}
+            ></div>
           </div>
           <div className="row3">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 6);
+              }}
+            ></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 7);
+              }}
+            ></div>
+            <div
+              className="boxes"
+              onClick={(e) => {
+                toggle(e, 8);
+              }}
+            ></div>
           </div>
         </div>
         {/* button section */}
