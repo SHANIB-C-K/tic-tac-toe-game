cb59c2a6e04bcb4cad0569e31227ad510432aa56
SHANIB C K <ckshanib8@gmail.com>
2024-03-10 09:46:05 +0530

    structure created

--- src/TicTacToe/TicTacToe.jsx
+++ src/TicTacToe/TicTacToe.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import cross_icon from "../assets/cross.png";
+import circle_icon from "../assets/circle.png";
 
 const TicTacToe = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="container">
+        <h1 className="title">
+          Tic Tac Toe <span>Game</span>
+        </h1>
+        {/* board section */}
+        <div className="board">
 
-export default TicTacToe
+        </div>
+        {/* button section */}
+        <button className="reset">Reset</button>
+      </div>
+    </>
+  );
+};
+
+export default TicTacToe;
