d78938b955bca2441ccbe6d86244b3d17cbdff6c
SHANIB C K <ckshanib8@gmail.com>
2024-03-10 11:25:00 +0530

    game created successfully

--- src/TicTacToe/TicTacToe.jsx
+++ src/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import cross_icon from "../assets/cross.png";
 import circle_icon from "../assets/circle.png";
 import "./TicTacToe.css";
@@ -8,6 +8,18 @@ let data = ["", "", "", "", "", "", "", "", ""];
 const TicTacToe = () => {
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
+  let titleRef = useRef(null);
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  let box3 = useRef(null);
+  let box4 = useRef(null);
+  let box5 = useRef(null);
+  let box6 = useRef(null);
+  let box7 = useRef(null);
+  let box8 = useRef(null);
+  let box9 = useRef(null);
+
+  let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
   //   toggle function created section
   const toggle = (e, num) => {
@@ -23,12 +35,55 @@ const TicTacToe = () => {
       data[num] = "o";
       setCount(++count);
     }
+    CheckWin();
+  };
+
+  //   check winn function create section
+  const CheckWin = () => {
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      win(data[2]);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      win(data[5]);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      win(data[8]);
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      win(data[6]);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      win(data[7]);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      win(data[8]);
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      win(data[8]);
+    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      win(data[2]);
+    } else if (data[2] === data[7] && data[7] === data[6] && data[6] !== "") {
+      win(data[6]);
+    }
+  };
+
+  const win = (winner) => {
+    setLock(true);
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon}>`;
+    } else {
+      titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon}>`;
+    }
+  };
+
+  // reset function create section
+  const Reset = () => {
+    setLock(false);
+    data = ["", "", "", "", "", "", "", "", ""];
+    titleRef.current.innerHTML = "Tic Tac Toe <span>Game</span>";
+    box_array.map((e) => {
+      e.current.innerHTML = "";
+    });
   };
 
   return (
     <>
       <div className="container">
-        <h1 className="title">
+        <h1 className="title" ref={titleRef}>
           Tic Tac Toe <span>Game</span>
         </h1>
         {/* board section */}
@@ -36,18 +91,21 @@ const TicTacToe = () => {
           <div className="row1">
             <div
               className="boxes"
+              ref={box1}
               onClick={(e) => {
                 toggle(e, 0);
               }}
             ></div>
             <div
               className="boxes"
+              ref={box2}
               onClick={(e) => {
                 toggle(e, 1);
               }}
             ></div>
             <div
               className="boxes"
+              ref={box3}
               onClick={(e) => {
                 toggle(e, 2);
               }}
@@ -56,18 +114,21 @@ const TicTacToe = () => {
           <div className="row2">
             <div
               className="boxes"
+              ref={box4}
               onClick={(e) => {
                 toggle(e, 3);
               }}
             ></div>
             <div
               className="boxes"
+              ref={box5}
               onClick={(e) => {
                 toggle(e, 4);
               }}
             ></div>
             <div
               className="boxes"
+              ref={box6}
               onClick={(e) => {
                 toggle(e, 5);
               }}
@@ -76,18 +137,21 @@ const TicTacToe = () => {
           <div className="row3">
             <div
               className="boxes"
+              ref={box7}
               onClick={(e) => {
                 toggle(e, 6);
               }}
             ></div>
             <div
               className="boxes"
+              ref={box8}
               onClick={(e) => {
                 toggle(e, 7);
               }}
             ></div>
             <div
               className="boxes"
+              ref={box9}
               onClick={(e) => {
                 toggle(e, 8);
               }}
@@ -96,7 +160,14 @@ const TicTacToe = () => {
         </div>
         {/* button section */}
         <div style={{ marginBottom: "50px" }}>
-          <button className="reset">Reset</button>
+          <button
+            className="reset"
+            onClick={() => {
+              Reset();
+            }}
+          >
+            Reset
+          </button>
         </div>
       </div>
     </>
