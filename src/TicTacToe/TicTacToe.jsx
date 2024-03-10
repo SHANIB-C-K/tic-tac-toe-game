801823d3364eab3895e3160608dbc02792893727
SHANIB C K <ckshanib8@gmail.com>
2024-03-10 10:17:24 +0530

    😊

--- src/TicTacToe/TicTacToe.jsx
+++ src/TicTacToe/TicTacToe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cross_icon from "../assets/cross.png";
 import circle_icon from "../assets/circle.png";
+import "./TicTacToe.css";
 
 const TicTacToe = () => {
   return (
@@ -11,10 +12,26 @@ const TicTacToe = () => {
         </h1>
         {/* board section */}
         <div className="board">
-
+          <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
         </div>
         {/* button section */}
-        <button className="reset">Reset</button>
+        <div style={{ marginBottom: "50px" }}>
+          <button className="reset">Reset</button>
+        </div>
       </div>
     </>
   );
